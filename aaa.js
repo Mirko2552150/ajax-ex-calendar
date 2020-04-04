f4eb4e2ef979e25b2c872433843fe3579a44bf17
Mirko <mirko.longo.1987@gmail.com>
2020-04-04 15:23:03 +0200

    Ok

---
+++ aaa.js
@@ -0,0 +1,33 @@
+// moment().format();
+// // console.log(moment().format());
+
+// var now = moment().locale('it').format("dddd"); // STAMPIANO IL GG CORRENTE si configura con la data del momento della generazione
+// console.log(now);
+//
+// var now2 = moment();
+// console.log(now2.month() + 1 ); // mettiamo il + 1 perchè si parte da 0
+//
+//
+// var dataAttuale = moment();
+// // console.log(dataAttuale);
+//
+// var dataAttuale = moment();
+// var dataAttuale1 = dataAttuale;
+// var dataAttualeMoment = moment(dataAttuale);
+// var dataAttualeClone = dataAttuale.clone();
+//
+// console.log(dataAttuale1.format('DD')); // stampa il giorno corrente
+//
+// dataAttuale.add(7, "days");
+// console.log(dataAttuale.format('DD')); // stampa il giorno corrente +7 days
+// console.log(dataAttuale1.format('DD')); // stampa il giorno corrente +7 days
+// console.log(dataAttualeMoment.format('DD')); // stampa il giorno corrente
+// console.log(dataAttualeClone.format('DD')); // stampa il giorno corrente
+//
+// var fineMese  = moment().endOf("month"); // ci indica il giono finale del mese
+// console.log(fineMese);
+//
+// var prova = moment('2010-10-20').isBetween('2010-01-01', '2012-01-01', 'year'); //false il 2010 NON è tra il 2010 e il 2012
+// var prova2 = moment('2010-10-20').isBetween('2009-12-31', '2012-01-01', 'year'); //true il 2010 è tra il 2009 e il 2012
+// console.log(prova);
+// console.log(prova2);
--- js/main.js
+++ js/main.js
@@ -1,71 +1,55 @@
-// moment().format();
-// // console.log(moment().format());
-
-// var now = moment().locale('it').format("dddd"); // STAMPIANO IL GG CORRENTE si configura con la data del momento della generazione
-// console.log(now);
-//
-// var now2 = moment();
-// console.log(now2.month() + 1 ); // mettiamo il + 1 perchè si parte da 0
-//
-//
-// var dataAttuale = moment();
-// // console.log(dataAttuale);
-//
-// var dataAttuale = moment();
-// var dataAttuale1 = dataAttuale;
-// var dataAttualeMoment = moment(dataAttuale);
-// var dataAttualeClone = dataAttuale.clone();
-//
-// console.log(dataAttuale1.format('DD')); // stampa il giorno corrente
-//
-// dataAttuale.add(7, "days");
-// console.log(dataAttuale.format('DD')); // stampa il giorno corrente +7 days
-// console.log(dataAttuale1.format('DD')); // stampa il giorno corrente +7 days
-// console.log(dataAttualeMoment.format('DD')); // stampa il giorno corrente
-// console.log(dataAttualeClone.format('DD')); // stampa il giorno corrente
-//
-// var fineMese  = moment().endOf("month"); // ci indica il giono finale del mese
-// console.log(fineMese);
-//
-// var prova = moment('2010-10-20').isBetween('2010-01-01', '2012-01-01', 'year'); //false il 2010 NON è tra il 2010 e il 2012
-// var prova2 = moment('2010-10-20').isBetween('2009-12-31', '2012-01-01', 'year'); //true il 2010 è tra il 2009 e il 2012
-// console.log(prova);
-// console.log(prova2);
 
 var htmlGiorno = $('#day-template').html();
 var templateGiorno = Handlebars.compile(htmlGiorno); // mettiamo la SOURCE ED IL TEPLATE PARLANTI
 // stiampiamo il mese corrente
 var dataIniziale = moment('2018-01-01'); // inseriamo la data da cui vogliamo partire
-// console.log(dataIniziale);
+console.log(dataIniziale.month());
 stampaGiorniMese(dataIniziale); // mi stampo i giorni ed il mese corrente
 // a seguito del click stampare il mese successivo
+stampaFestivi(0, 2018); // lo faccio partire dal mese 0 = gennaio
+
 stampaFestivi();
 $('.succ').click(function (){ // al click sul bottone succ
     dataIniziale.add(1 , "months"); // aggiungo un mese al mese corrente
     // console.log(dataIniziale);
-    stampaGiorniMese(dataIniziale)
-    console.log(dataIniziale);
-    var meseAjax = dataIniziale.clone().format('mm');
-    console.log(meseAjax);
-    stampaFestivi();
+    stampaGiorniMese(dataIniziale);
+    // console.log(dataIniziale);
+    console.log(dataIniziale.month());
+    var mese = dataIniziale.month();
+    var anno = dataIniziale.year();
+    stampaFestivi(mese, anno);
+    if (anno == 2019) { // se trovo l'anno 2019
+        $('body').empty(); // svuoto il body
+        alert("NON SEI NELL'ANNO CORRETTO"); // esce l'alert
+        location.reload(); // ricarica le condizioni iniziali della pagina
 
+    }
 });
 // a seguito del click stampare il mese precedente
 $('.prev').click(function (){ // al click sul bottone succ
     dataIniziale.subtract(1 , "months"); // tolgo un mese al mese corrente
     // console.log(dataIniziale);
     stampaGiorniMese(dataIniziale);
-    // stampaFestivi(meseAjax);
 
+    console.log(dataIniziale.month());
+    var mese = dataIniziale.month();
+    var anno = dataIniziale.year();
+    stampaFestivi(mese, anno);
+    if (anno == 2017) { // se trovo il
+        $('body').empty(); // svuoto il body
+        alert("NON SEI NELL'ANNO CORRETTO"); // esce l'alert
+        location.reload(); // ricarica le condizioni iniziali della pagina
+
+    }
 });
 
-function stampaFestivi(){ // usiamo una funzione che procedera a fare un chiamata AJAX per richiamare i gg festivi dell'anno 2018
+function stampaFestivi(mes, ann) { // usiamo una funzione che procedera a fare un chiamata AJAX per richiamare i gg festivi dell'anno 2018
     $.ajax({
         url: 'https://flynn.boolean.careers/exercises/api/holidays',
         method: 'GET',
         data: {
-            year: 2018,
-            month: 0
+            year: ann,
+            month: mes
         },
         success: function (data) { // al successo della chiamata
             // console.log(data);
@@ -77,10 +61,9 @@ function stampaFestivi(){ // usiamo una funzione che procedera a fare un chiamat
                 var dataFestivo = festivo.date;
                 // console.log(nomeFestivo);
                 // console.log(dataFestivo);
-                $('.calendar span[data-day="' + dataFestivo + '"]').addClass('festivo').append('<div>' + nomeFestivo + '</div>');
+                $('.calendar span[data-day="' + dataFestivo + '"]').addClass('festivo').append('<div>' + nomeFestivo + '</div>'); // prendi lo span quando equivalente Push il contenuto dell'oggetto
             }
-
-        }
+        },
     });
 }
 
